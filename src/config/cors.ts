5461b90eae95cba132f4fd97499d5e38e6bd07a0
Abhilaksh <abhilaksh2095@gmail.com>
2024-12-09 00:38:53 +0530

    cors error

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,15 +3,15 @@ import { cors } from "hono/cors";
 
 config();
 
-const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
-  ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:4000", "*"];
+// Allow all origins
+const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS?.split(",") || ["*"];
 
 const corsConfig = cors({
-  allowMethods: ["GET"],
-  maxAge: 600,
-  credentials: true,
   origin: allowedOrigins,
+  allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowHeaders: ["Content-Type", "Authorization"],
+  maxAge: 600,
+  credentials: true, // Allow credentials if needed
 });
 
 export default corsConfig;
