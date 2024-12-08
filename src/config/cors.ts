5f839c11923215c2a35fa0c5dc9805a95c3adc98
Abhilaksh <abhilaksh2095@gmail.com>
2024-12-09 00:45:24 +0530

    cors again

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,10 +4,8 @@ import { cors } from "hono/cors";
 config();
 
 // Allow all origins
-const allowedOrigins = ["*"]; // Replace "*" with specific origins if needed
-
 const corsConfig = cors({
-  origin: allowedOrigins,
+  origin: "*",
   allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowHeaders: ["Content-Type", "Authorization"],
   maxAge: 600,
