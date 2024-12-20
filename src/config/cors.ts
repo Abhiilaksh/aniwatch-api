a0c9c4d6bf2ef093df00ea3d95e9bbfc68723a0c
Abhilaksh Thakur <135262451+Abhiilaksh@users.noreply.github.com>
2024-12-20 17:18:54 +0530

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,15 +3,11 @@ import { cors } from "hono/cors";
 
 config();
 
-const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
-  ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:4000", "*"];
-
 const corsConfig = cors({
-  allowMethods: ["GET"],
-  maxAge: 600,
-  credentials: true,
-  origin: *,
+  allowMethods: ["GET"], // Specifies the HTTP methods allowed
+  maxAge: 600, // Specifies the maximum amount of time (in seconds) the preflight request can be cached
+  credentials: true, // Allows sending credentials like cookies or authorization headers
+  origin: "*", // Allows requests from all origins
 });
 
 export default corsConfig;
