75b7d018b23db82d5fb22d27ec7f55c958db3874
Abhilaksh Thakur <135262451+Abhiilaksh@users.noreply.github.com>
2025-01-16 22:14:57 +0530

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -7,7 +7,7 @@ const corsConfig = cors({
   allowMethods: ["GET"], // Specifies the HTTP methods allowed
   maxAge: 600,           // Specifies the maximum time (in seconds) the preflight request can be cached
   credentials: true,     // Allows sending credentials like cookies or authorization headers
-  origin: "https://anigalaxy.xyz", // Allows requests only from https://anigalaxy.xyz
+  origin: "https://www.anigalaxy.xyz", // Allows requests only from https://anigalaxy.xyz
 });
 
 export default corsConfig;
