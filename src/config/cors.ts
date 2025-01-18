54e97b94a7241ff8655c7b8c9a6f0f1de4727192
Abhilaksh Thakur <135262451+Abhiilaksh@users.noreply.github.com>
2025-01-18 22:52:33 +0530

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,10 +4,10 @@ import { cors } from "hono/cors";
 config();
 
 const corsConfig = cors({
-  allowMethods: ["GET"], // Specifies the HTTP methods allowed
-  maxAge: 600,           // Specifies the maximum time (in seconds) the preflight request can be cached
-  credentials: true,     // Allows sending credentials like cookies or authorization headers
-  origin: "https://www.anigalaxy.xyz", // Allows requests only from https://anigalaxy.xyz
+  allowMethods: ["GET"],
+  maxAge: 600,
+  credentials: true,
+  origin: ["https://www.anigalaxy.xyz", "http://localhost:5173"]  // Fixed: Now in an array
 });
 
 export default corsConfig;
